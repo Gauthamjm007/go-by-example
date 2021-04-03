ebe8fb4a4c9c94266cbea77cb785ccfb8d7884b7
Gauthamjm007 <j.mgautham15@gmail.com>
2021-04-03 23:41:20 +0530

    updating quick sort

---
+++ Algorithms/Sort/QuickSort/quicksort.go
@@ -0,0 +1,27 @@
+package quicksort
+
+import "fmt"
+
+var timer = 0
+
+func quicksort(data []int) {
+	if len(data) <= 1 {
+		return
+	}
+	mid := data[0]
+	head, tail := 0, len(data)-1
+	for i := 1; i <= tail; {
+		timer++
+		if data[i] > mid {
+			data[i], data[tail] = data[tail], data[i]
+			tail--
+		} else {
+			data[i], data[head] = data[head], data[i]
+			head++
+			i++
+		}
+	}
+	quicksort(data[:head])
+	quicksort(data[head+1:])
+	fmt.Println(timer, "times")
+}
---
+++ Algorithms/Sort/QuickSort/quicksort_test.go
@@ -0,0 +1,29 @@
+package quicksort
+
+import "testing"
+
+func TestQuickSort(t *testing.T) {
+
+	tests := []struct {
+		name string
+		arr  []int
+	}{
+		{
+			name: "test-1",
+			arr:  []int{9, 8, 7, 6, 5, 4, 3, 2, 1, 0},
+		},
+		{
+			name: "test-2",
+			arr:  []int{2, 3, 1},
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			t.Log("Before select sort", tt.arr)
+			quicksort(tt.arr)
+			t.Log("After select sort", tt.arr)
+		})
+	}
+
+}
